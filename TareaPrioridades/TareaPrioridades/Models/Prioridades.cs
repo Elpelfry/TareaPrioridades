c32b5be851e9e22a2421d1ff38130c36235f188a
Delfry Paulino Ortega <delfry_paulino@ucne.edu.do>
2024-01-11 00:09:56 -0400

    Models Agregué el modelo Prioridades

---
+++ TareaPrioridades/TareaPrioridades/Models/Prioridades.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TareaPrioridades.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+
+        [Required(ErrorMessage = "Este Campo es Obligatorio")]
+        public string Descripcion { get; set; }
+
+        [Range(1, 31, ErrorMessage = "El valor debe estar entre 1 y 31")]
+        public int DiasCompromiso { get; set; }
+    }
+}
