93855764a505f69dcc0d45a4a54cfe601ffa03d1
Delfry Paulino Ortega <delfry_paulino@ucne.edu.do>
2024-01-11 00:14:08 -0400

    Agrego el Contexto

---
+++ TareaPrioridades/TareaPrioridades/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TareaPrioridades.Models;
+
+namespace TareaPrioridades.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Prioridades> Prioridades { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    }
+}
