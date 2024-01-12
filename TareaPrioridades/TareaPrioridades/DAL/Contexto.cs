02e3b6c28c08b4a4c90cb44fbc8a61f7e69febe2
Delfry Paulino Ortega <delfry_paulino@ucne.edu.do>
2024-01-11 20:38:35 -0400

    Mas cambios metodos

--- TareaPrioridades/TareaPrioridades/DAL/Contexto.cs
+++ TareaPrioridades/TareaPrioridades/DAL/Contexto.cs
@@ -6,7 +6,6 @@ namespace TareaPrioridades.DAL
     public class Contexto : DbContext
     {
         public DbSet<Prioridades> Prioridades { get; set; }
-
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
     }
 }
