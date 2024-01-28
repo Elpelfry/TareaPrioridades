b02aef862cab10e45391db56f065cb72a35fea99
Delfry Paulino Ortega <delfry_paulino@ucne.edu.do>
2024-01-28 13:00:34 -0400

    Pagina de clientes y modelos Sistemas y Tickets

---
+++ TareaPrioridades/TareaPrioridades/Models/Sistemas.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TareaPrioridades.Models
+{
+    public class Sistemas
+    {
+        [Key]
+        public int SistemaId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? NombreSistema { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? DescripcionSistema { get; set; }
+    }
+}
---
+++ TareaPrioridades/TareaPrioridades/Models/Tickets.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TareaPrioridades.Models
+{
+    public class Tickets
+    {
+        [Key]
+        public int TicketId { get; set; }
+        [Required(ErrorMessage = "El campo fecha es obligatorio")]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "El campo Cliente es obligatorio")]
+        [ForeignKey("Clientes")]
+        public int ClienteId { get; set; }
+
+        [ForeignKey("Sistemas")]
+        public int SistemaId { get; set; }
+
+        [ForeignKey("Prioridades")]
+        public int PrioridadId { get; set; }
+
+        [Required(ErrorMessage = "El campo Solicitado por es obligatorio")]
+        [RegularExpression("^[a-zA-Z ]+$", ErrorMessage = "Solo debe contener letras.")]
+        public string? Solicitadopor { get; set; }
+
+        [Required(ErrorMessage = "El campo Asunto es obligatorio")]
+        public string? Asunto { get; set; }
+
+        [Required(ErrorMessage = "El campo Descripción es obligatorio")]
+        public string? Descripcion { get; set; }
+    }
+}
